71e66114108d94c76f6f3491368b01c948393166
asquaredigital <asquaredigitalsolutions@gmail.com>
2024-04-27 16:43:49 +0530

    commit

---
+++ mailtest/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// AWS credentials
+return [
+    'aws' => [
+        'key' => 'AKIA4B6MYLBT4VUVKUHQ',
+        'secret' => '90RP/z/q18Ic7v+jnSYErwzFtTfIUEsjUkiON+Z5',
+        'region' => 'ap-south-1', // Change to your desired AWS region
+    ],
+];
